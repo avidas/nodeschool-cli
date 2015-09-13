965bb77024f7aa50c6d698ed938fb7721704761a
Avi Das <andas222@gmail.com>
2015-09-12 19:24:25 -0500

    Get nodeschool homepage, parse modules, check if modules are installed in the system

--- README.md
+++ README.md
@@ -1,2 +1,49 @@
 # nodeschool-cli
-View available nodeschool modules and the ones installed in your system
+View available nodeschool modules and the ones installed in your system.
+
+[![Build status](https://travis-ci.org/avidas/nodeschool-cli.svg?branch=master)](https://travis-ci.org/avidas/nodeschool-cli)
+
+## Installation
+
+```bash
+npm install -g nodeschool-cli
+```
+
+to get the `nodeschool-cli` command.
+
+## Usage
+
+```
+nodeschool-cli --list
+
+  Prints which nodeschool modules are installed and not installed on your system.
+
+nodeschool-cli --list
+nodeschool-cli -l
+
+  List all the nodeschool modules installed in the system
+```
+
+```
+introtowebgl NOT INSTALLED
+browserify-adventure INSTALLED
+bug-clinic INSTALLED
+bytewiser INSTALLED
+shader-school NOT INSTALLED
+regex-adventure INSTALLED
+learnyoumongo INSTALLED
+tower-of-babel INSTALLED
+test-anything NOT INSTALLED
+.
+.
+
+```
+
+## TODO
+-- cache the module names
+-- install all?
+
+## License
+
+MIT
+
---
+++ bin/cmd.js
@@ -0,0 +1,60 @@
+#!/usr/bin/env node
+
+var fs = require('fs');
+var http = require("http");
+var minimist = require('minimist');
+var exec = require('child_process').exec;
+var cheerio = require("cheerio");
+
+var argv = minimist(process.argv.slice(2), {
+    alias: {
+        l: [ 'ls', 'list'],
+        h: 'help',
+        i: 'install'
+    }
+});
+
+var _url = "http://nodeschool.io/";
+
+function _puts(error, stdout, stderr) { 
+    process.stdout.write(stdout);
+}
+
+function _isOptionalArg(value) {
+  return value.indexOf('-') !== 0
+}
+
+function _download (url, callback) {
+  http.get(url, function(res) {
+    var data = "";
+    res.on('data', function (chunk) {
+      data += chunk;
+    });
+    res.on("end", function() {
+      callback(data);
+    });
+  }).on("error", function() {
+    callback(null);
+  });
+}
+
+if (argv.help) {
+    return fs.createReadStream(__dirname + '/usage.txt')
+        .pipe(process.stdout);
+}
+
+if (argv.list) {
+    _download(_url, function (data) {
+        if (data) {
+            var $ = cheerio.load(data);
+            $('.workshopper[id] code').each(function (i, e) {
+                var arr = $(this).text().split(' ').slice(2);
+                var moduleName = arr.filter(_isOptionalArg)[0];
+                var existsPath = 'if [ -x "$(command -v ' + moduleName + ')" ]; then echo "' + moduleName + ' INSTALLED" >&1; else echo "' + moduleName + ' NOT INSTALLED" >&1; fi';
+                exec(existsPath, _puts);
+            });
+        } else {
+            console.error('Could not download the url.')
+        }
+    });
+}
